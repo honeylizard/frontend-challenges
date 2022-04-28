1e25619e4b29c6856aac574d47567498fb6bcece
Lisa Bernhardt <honeylizard@users.noreply.github.com>
2022-04-28 17:15:37 -0400

    load country list items under Suspense

--- ui/src/components/CountriesList/CountriesList.js
+++ ui/src/components/CountriesList/CountriesList.js
@@ -1,13 +1,14 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, Suspense } from "react";
 import PropTypes from "prop-types";
 import { injectIntl } from "react-intl";
 import lodash from "lodash";
 import { GlobalContext } from "../../GlobalStateProvider";
 import { axiosGet } from "./utils/api-helper";
-import CountriesListItem from "./CountriesListItem";
 import countriesListStyle from "../../styles/countries-api/countries-list.module.scss";
 import Loading from "./common/Loading";
 import CountriesFilterForm from "./CountriesFilterForm";
+
+const CountriesListItem = React.lazy(() => import("./CountriesListItem"));
 
 const CountriesList = ({ intl }) => {
     const { countriesApi: globalData } = useContext(GlobalContext);
@@ -58,6 +59,22 @@ const CountriesList = ({ intl }) => {
         setFilteredRecords(lodash.orderBy(filterByRegion, ["name"], ["asc"]));
     }, [currentFilters, records]);
 
+    const renderPlaceholderItem = () => {
+        const placeholderImage =
+            process.env.PUBLIC_URL + "/assets/flag_placeholder.jpg";
+        const contentLabel = intl.formatMessage({
+            id: "countriesApi.countries.loadingSingle",
+        });
+        return (
+            <div className={countriesListStyle.placeholderBox}>
+                <img src={placeholderImage} alt="" role="presentation" />
+                <div className={countriesListStyle.placeholderBoxContent}>
+                    {contentLabel}
+                </div>
+            </div>
+        );
+    };
+
     const renderList = () => {
         return (
             <React.Fragment>
@@ -69,7 +86,9 @@ const CountriesList = ({ intl }) => {
                                 key={`country-${index}`}
                                 className={listItemClasses.join(" ")}
                             >
-                                <CountriesListItem data={country} />
+                                <Suspense fallback={renderPlaceholderItem()}>
+                                    <CountriesListItem data={country} />
+                                </Suspense>
                             </li>
                         ))}
                     </ul>
