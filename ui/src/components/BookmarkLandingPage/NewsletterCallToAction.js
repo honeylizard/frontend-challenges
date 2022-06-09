5189ab10e7988ae390b09b02203afcd6f1470f72
Lisa Bernhardt <honeylizard@users.noreply.github.com>
2022-06-09 15:52:21 -0400

    initial styling for cta

--- ui/src/components/BookmarkLandingPage/NewsletterCallToAction.js
+++ ui/src/components/BookmarkLandingPage/NewsletterCallToAction.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { injectIntl } from "react-intl";
 import pageStyles from "../../styles/bookmark-landing-page/page.module.scss";
+import Button from "./common/Button";
 
 const NewsletterCallToAction = ({ intl }) => {
     // TODO: setup form submission logic and validation
@@ -10,15 +11,29 @@ const NewsletterCallToAction = ({ intl }) => {
         <section className={pageStyles.callToAction}>
             <div className={pageStyles.wrapper}>
                 <div className={pageStyles.reverseHeaderOrder}>
-                    <h2>Stay up-to-date with what we&apos;re doing</h2>
-                    <h3>35,000+ Already joined</h3>
+                    <h2 className={pageStyles.callToActionTitle}>
+                        Stay up-to-date with what we&apos;re doing
+                    </h2>
+                    <h3 className={pageStyles.callToActionSubtitle}>
+                        35,000+ Already joined
+                    </h3>
                 </div>
                 <form>
                     <label htmlFor="newsletter-email" className="sr-only">
                         Email
                     </label>
-                    <input id="newsletter-email" type="email" />
-                    <button type="submit">Contact Us</button>
+                    <input
+                        id="newsletter-email"
+                        type="email"
+                        placeholder="Enter your email address"
+                    />
+                    <Button
+                        type="submit"
+                        rank="tertiary"
+                        customClasses={[pageStyles.callToActionSubmit]}
+                    >
+                        Contact Us
+                    </Button>
                 </form>
             </div>
         </section>
--- ui/src/components/BookmarkLandingPage/common/Button.js
+++ ui/src/components/BookmarkLandingPage/common/Button.js
@@ -2,8 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import buttonStyles from "../../../styles/bookmark-landing-page/button.module.scss";
 
-const Button = ({ label, children, customClasses = [], ...attrs }) => {
-    const classes = [buttonStyles.button, ...customClasses];
+const Button = ({ label, children, rank, customClasses = [], ...attrs }) => {
+    const classes = [
+        buttonStyles.button,
+        rank === "primary" ? buttonStyles.buttonPrimary : null,
+        rank === "secondary" ? buttonStyles.buttonSecondary : null,
+        rank === "tertiary" ? buttonStyles.buttonTertiary : null,
+        ...customClasses,
+    ].filter(Boolean);
 
     return (
         <button className={classes.join(" ")} {...attrs}>
@@ -15,6 +21,7 @@ const Button = ({ label, children, customClasses = [], ...attrs }) => {
 Button.propTypes = {
     label: PropTypes.string,
     children: PropTypes.node,
+    rank: PropTypes.string,
     customClasses: PropTypes.array,
 };
 
