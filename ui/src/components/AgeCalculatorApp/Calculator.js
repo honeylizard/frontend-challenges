29a2da7f6f77e6557beae088135dbd07b4e85a5e
Lisa Bernhardt <honeylizard@users.noreply.github.com>
2024-04-16 23:04:37 -0400

    initial styling

--- ui/src/components/AgeCalculatorApp/Calculator.js
+++ ui/src/components/AgeCalculatorApp/Calculator.js
@@ -1,16 +1,74 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { injectIntl } from "react-intl";
+import { injectIntl, FormattedPlural } from "react-intl";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import { debounce } from "lodash";
 
-const INITIAL_RESULTS = { days_ago: "-", months_ago: "-", years_ago: "-" };
+import Button from "../CountriesList/common/Button";
+
+import arrowIcon from "../../assets/age-calculator-app/icon-arrow.svg";
+
+import styles from "../../styles/age-calculator-app/app.module.scss";
+
+const INITIAL_RESULTS = { days_ago: "--", months_ago: "--", years_ago: "--" };
 
 const Calculator = ({ intl }) => {
     // Set of form fields and "hidden" fields that will be passed through when submitted
     const [formData, setFormData] = useState({ day: null, month: null, year: null });
     const [results, setResults] = useState(INITIAL_RESULTS);
+    const formFields = [
+        {
+            id: "day",
+            type: "number",
+            label: intl.formatMessage({ id: "ageCalculatorApp.day" }),
+            placeholder: "DD",
+        },
+        {
+            id: "month",
+            type: "number",
+            label: intl.formatMessage({ id: "ageCalculatorApp.month" }),
+            placeholder: "MM",
+        },
+        {
+            id: "year",
+            type: "number",
+            label: intl.formatMessage({ id: "ageCalculatorApp.year" }),
+            placeholder: "YYYY",
+        },
+    ];
+    const outputFields = [
+        {
+            id: 1,
+            value: results.years_ago,
+            label: {
+                zero: intl.formatMessage({ id: "ageCalculatorApp.years" }),
+                one: intl.formatMessage({ id: "ageCalculatorApp.year" }),
+                other: intl.formatMessage({ id: "ageCalculatorApp.years" }),
+            },
+        },
+        {
+            id: 2,
+            value: results.months_ago,
+            label: {
+                zero: intl.formatMessage({ id: "ageCalculatorApp.months" }),
+                one: intl.formatMessage({ id: "ageCalculatorApp.month" }),
+                other: intl.formatMessage({ id: "ageCalculatorApp.months" }),
+            },
+        },
+        {
+            id: 3,
+            value: results.days_ago,
+            label: {
+                zero: intl.formatMessage({ id: "ageCalculatorApp.days" }),
+                one: intl.formatMessage({ id: "ageCalculatorApp.day" }),
+                other: intl.formatMessage({ id: "ageCalculatorApp.days" }),
+            },
+        },
+    ];
+    const submitButtonLabel = intl.formatMessage({
+        id: "ageCalculatorApp.submit.label",
+    });
 
     const updateValue = (event) => {
         const { id, name, value: newValue } = event.target;
@@ -21,25 +79,7 @@ const Calculator = ({ intl }) => {
         }));
     };
 
-    const formFields = [
-        {
-            id: "day",
-            type: "number",
-            label: intl.formatMessage({ id: "ageCalculatorApp.day" }),
-        },
-        {
-            id: "month",
-            type: "number",
-            label: intl.formatMessage({ id: "ageCalculatorApp.month" }),
-        },
-        {
-            id: "year",
-            type: "number",
-            label: intl.formatMessage({ id: "ageCalculatorApp.year" }),
-        },
-    ];
-
-    useEffect(() => {
+    const handleSubmit = () => {
         if (formData.year > 0 && formData.month > 0 && formData.day > 0) {
             dayjs.extend(relativeTime);
 
@@ -72,8 +112,6 @@ const Calculator = ({ intl }) => {
             });
             */
 
-            console.log("new results");
-
             setResults({
                 days_ago: Math.floor(monthsRemainderAsDays),
                 months_ago: Math.floor(yearsRemainderAsMonths),
@@ -85,31 +123,55 @@ const Calculator = ({ intl }) => {
                 setResults(INITIAL_RESULTS);
             }
         }
-    }, [formData]); // eslint-disable-line react-hooks/exhaustive-deps
-
-    console.log("results", results);
+    };
 
     return (
-        <div>
+        <div className={styles.card}>
             <form>
-                {formFields.map((field) => (
-                    <div key={`input-${field.id}`}>
-                        <label htmlFor={field.id}>{field.label}</label>
-                        <input id={field.id} type={field.type || "text"} onChange={debounce(updateValue, 500)} />
-                    </div>
-                ))}
+                <div className={styles.form}>
+                    {formFields.map((field) => (
+                        <div key={`input-${field.id}`} className={styles.field}>
+                            <label className={styles.label} htmlFor={field.id}>
+                                {field.label}
+                            </label>
+                            <input
+                                id={field.id}
+                                className={styles.input}
+                                type={field.type || "text"}
+                                onChange={debounce(updateValue, 500)}
+                                placeholder={field.placeholder}
+                            />
+                        </div>
+                    ))}
+                </div>
+                <div className={styles.divider}>
+                    <div className={styles.line} />
+                    <Button
+                        type="submit"
+                        customClasses={[styles.submitButton]}
+                        aria-label={submitButtonLabel}
+                        onClick={handleSubmit}
+                    >
+                        <img src={arrowIcon} alt="" role="presentation" />
+                    </Button>
+                </div>
             </form>
-
-            <output htmlFor={formFields.map((field) => field.id).join(" ")}>
-                <div>
-                    {results.years_ago} {intl.formatMessage({ id: "ageCalculatorApp.years" })}
-                </div>
-                <div>
-                    {results.months_ago} {intl.formatMessage({ id: "ageCalculatorApp.months" })}
-                </div>
-                <div>
-                    {results.days_ago} {intl.formatMessage({ id: "ageCalculatorApp.days" })}
-                </div>
+            <output className={styles.output} htmlFor={formFields.map((field) => field.id).join(" ")}>
+                {outputFields.length > 0 &&
+                    outputFields.map((field) => (
+                        <div key={`output-${field.id}`}>
+                            <span className={styles.number}>{field.value}</span>
+                            &nbsp;
+                            <span className={styles.label}>
+                                <FormattedPlural
+                                    value={field.value}
+                                    one={field.label.one}
+                                    other={field.label.other}
+                                    zero={field.label.zero}
+                                />
+                            </span>
+                        </div>
+                    ))}
             </output>
         </div>
     );
--- ui/src/components/AgeCalculatorApp/Footer.js
+++ ui/src/components/AgeCalculatorApp/Footer.js
@@ -1,29 +1,18 @@
-import React, { useContext } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { injectIntl, FormattedMessage } from "react-intl";
-
-import { GlobalContext } from "../../GlobalStateProvider";
 
 import appStyles from "../../styles/age-calculator-app/app.module.scss";
 
 const Footer = ({ intl }) => {
-    const { calculatorApp: globalData } = useContext(GlobalContext);
-    const currentTheme = globalData.theme;
-
     const challengeLinkUrl = intl.formatMessage({ id: "footer.challenge.url" });
     const challengeLinkLabel = intl.formatMessage({
         id: "footer.challenge.name",
     });
     const codedBy = intl.formatMessage({ id: "footer.codedBy" });
 
-    const themeLabel = intl.formatMessage({
-        id: "calculatorApp.theme",
-    });
-
     return (
         <footer className={[appStyles.wrapper, appStyles.footer].join(" ")}>
-            {themeLabel}: {currentTheme}
-            <br />
             <FormattedMessage
                 id="footer.challenge.link"
                 values={{
