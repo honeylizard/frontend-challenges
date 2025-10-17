90e2753e1dd62049d074c0955fc290d53e4eb1ac
Lisa Bernhardt <eliz.bernhardt@gmail.com>
2025-10-16 23:40:47 -0400

    add visualizer for vite build chunks

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig(() => {
     return {
@@ -10,6 +11,11 @@ export default defineConfig(() => {
         build: {
             outDir: 'build',
         },
-        plugins: [react()],
+        plugins: [
+            react(),
+            visualizer({
+                // open: true, // Automatically opens the visualizer in your browser
+            }),
+        ],
     };
 });
